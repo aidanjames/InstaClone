c166fe02bc78e3feaa991acbea5e8ca3d16ab3fc
AidanJP <aidanpendlebury@gmail.com>
2020-10-24 19:04:28 +0100

    Initial Commit

---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Aidan Pendlebury on 24/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
