da0464041ed371857786fd3baa9563305440901a
AidanJP <aidanpendlebury@gmail.com>
2020-10-30 20:30:36 +0000

    Added the heart animation when liking a post by double tapping the image.

--- InstaClone/Views/PostViews/PostListView.swift
+++ InstaClone/Views/PostViews/PostListView.swift
@@ -10,16 +10,30 @@ import SwiftUI
 struct PostListView: View {
     @ObservedObject var viewModel: FeedViewModel
     var post: Post
+    @State private var heartScale: CGFloat = 0
     
     var body: some View {
         VStack {
             PosterProfileView(post: post)
-            Image(post.postUrl)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .onTapGesture(count: 2) {
-                    viewModel.doubleTapped(post: post)
+            ZStack {
+                Image(post.postUrl)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .onTapGesture(count: 2) {
+                        viewModel.doubleTapped(post: post)
+                        withAnimation(.spring(response: 0.2, dampingFraction: 0.5, blendDuration: 2)) {
+                            heartScale = 2
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                heartScale = 0
+                            }
+                        }
                 }
+                Image(systemName: "heart.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .scaleEffect(heartScale)
+                
+            }
             VStack(alignment: .leading, spacing: 1) {
                 PostActionRibbonView(viewModel: viewModel, post: post)
                 PostCommentsView(post: post)
