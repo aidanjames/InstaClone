6e48b3bcfb622e54b1ab36a270fef6aa772bc375
AidanJP <aidanpendlebury@gmail.com>
2020-11-03 17:52:11 +0000

    Identified logic for filtering the stories feed.

--- InstaClone/Model/FeedViewModel.swift
+++ InstaClone/Model/FeedViewModel.swift
@@ -13,6 +13,7 @@ class FeedViewModel: ObservableObject {
     @Published var postsForMainFeed = [Post]()
     @Published var commentsForMainFeed = [PostComment]()
     @Published var postsForStoriesFeed = [Post]()
+    @Published var storiesToView = [[Post]]() // The nested array will be stories for a single user, whether they've been seen or not. The user to present first in the array should be the one with the most recently posted unseen story
     
     init(userName: String) {
         self.userName = userName
@@ -49,6 +50,15 @@ class FeedViewModel: ObservableObject {
         let newPost = Post(postedBy: DataUniverse.shared.allUsers[randomNumber].id, postType: .story, postUrl: "")
         DataUniverse.shared.addPostForUser(user: DataUniverse.shared.allUsers[randomNumber], post: newPost)
         updateUi()
+    }
+    
+    func sortStoriesForFeed() {
+        // 1. Create a variable of type [[Post]]
+        // 2. Fetch an array of all Posts of type .story and assign to a new array of type [Post]
+        // 3. Find the most recently created unseen story and identify the associated UserProfile
+        // 4. Get all stories for the UserProfile from 3 and populate into a new array of [Post], append this to the [[Popst]] array created in 1
+        // 5. Remove all these stories from the [Post] array in 2
+        // 6. Repeat steps 3-5 while array from 2 !.isEmpty
     }
     
     func updateUi() {
